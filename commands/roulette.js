0c97383d68e89cfb86b5c9b9ce2f88a532073216
DURGESHKS <80068736+DURGESHYS@users.noreply.github.com>
2021-03-26 19:10:59 +0530

    Update roulette.js

--- commands/roulette.js
+++ commands/roulette.js
@@ -3,7 +3,6 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
 
   let user = message.author;
 
